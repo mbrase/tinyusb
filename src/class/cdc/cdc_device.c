7dfe90d05ec18725f379e0d18a56331c3778d050
Michael Brase <mbrase@google.com>
2025-04-11 11:49:13 -0600

    Add new CDC device callback on driver reset
    This adds tud_cdc_reset_cb(), which can be implemented to receive
    notifications when the CDC driver is reset.

--- src/class/cdc/cdc_device.c
+++ src/class/cdc/cdc_device.c
@@ -301,6 +301,7 @@ void cdcd_reset(uint8_t rhport) {
       tu_fifo_clear(&p_cdc->tx_ff);
     }
     tu_fifo_set_overwritable(&p_cdc->tx_ff, true);
+    if (tud_cdc_reset_cb) tud_cdc_reset_cb(i);
   }
 }
 
